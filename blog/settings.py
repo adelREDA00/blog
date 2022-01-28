8eb66bc447756b70fc366d2a8e91659b98a19090
toumi reda <fifaadel65@gmail.com>
2022-01-28 23:46:46 +0100

    Allowed Hosts

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = 'django-insecure-sny8pl&42hp5=t%2jh01mp*@!4qti318xevk_5)dxka%q$@^6g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+    'fathomless-woodland-96117.herokuapp.com'
+]
 
 
 
