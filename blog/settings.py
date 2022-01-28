fe217827fcb32986b63281ba22a730761eb7acf8
toumi reda <fifaadel65@gmail.com>
2022-01-28 07:52:26 +0100

    lol

--- blog/settings.py
+++ blog/settings.py
@@ -83,11 +83,9 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config()
 }
+
 
 
 # Password validation
